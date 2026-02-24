d1fbe21564b799ab7305973f05cdef0121353c66
fwizzentainer-afk <fwizzentainer@gmail.com>
2026-02-24 05:44:37 +0000

    reset index clean

--- server/index.ts
+++ server/index.ts
@@ -101,3 +101,38 @@ app.use((req, res, next) => {
     },
   );
 })();
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+app.use(express.json());
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Dispositivo conectado:", socket.id);
+
+  socket.on("novo-pedido", (pedido) => {
+    console.log("Pedido recebido:", pedido);
+    io.emit("pedido-recebido", pedido);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Dispositivo desconectado");
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Servidor funcionando 🚀");
+});
+
+httpServer.listen(process.env.PORT || 3000, () => {
+  console.log("Servidor rodando");
+});
